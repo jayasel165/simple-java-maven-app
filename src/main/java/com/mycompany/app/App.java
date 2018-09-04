5a689e11144f6c4ae0908a0f3e1fda7b6f865d3e
jayasel165 <42759305+jayasel165@users.noreply.github.com>
2018-09-04 10:40:06 +0530

    "committed changes"

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World! my github modification done for automatic build test";
+    private final String message = "Hello World! my github modification for automatic build";
 
     public App() {}
 
