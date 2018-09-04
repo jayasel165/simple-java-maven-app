28cfa62535788d0a681263250d785dabb2268851
jayasel165 <42759305+jayasel165@users.noreply.github.com>
2018-09-04 10:19:32 +0530

    Automatic build

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World! my github modification";
+    private final String message = "Hello World! my github modification done automatic build";
 
     public App() {}
 
