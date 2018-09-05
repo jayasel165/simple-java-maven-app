527c25fc53f8ed0a14be729db3d56642c954c984
jayasel165 <42759305+jayasel165@users.noreply.github.com>
2018-09-05 09:03:10 +0530

    new commit changes

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World! my github automatic integration of github";
+    private final String message = "Hello World! my github automatic integration of git to test";
 
     public App() {}
 
