32e4b87699a221b00b654d0c899f83bee8b3fd10
jayasel165 <42759305+jayasel165@users.noreply.github.com>
2018-09-07 08:46:13 +0530

    commit

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World! my github testing done by great siva";
+    private final String message = "Hello World! my github testing done by great Vinoth";
 
     public App() {}
 
